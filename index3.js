dffa72141ba02c894043eb878c304b7fd704575c
vishnurejii <vishnurejii2003@gmail.com>
2026-02-19 11:31:44 +0530

    new commit

--- index3.js
+++ index3.js
@@ -1,6 +1,12 @@
 import express from "express"
 const app=express()
+
+app.get("/",(req,res)=>{
+    res.send("hello welcome")
+})
 app.get("/home",(req,res)=>{
     res.send("hello welcome to home")
 })
-app.listen(8080)
+app.listen(8080,()=>{
+    console.log("server listening")
+})
