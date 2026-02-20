527fcd48bc3dc73dcd51c5e00432071d0adc29dc
vishnurejii <vishnurejii2003@gmail.com>
2026-02-20 16:36:14 +0530

    new commit

--- index9e.js
+++ index9e.js
@@ -2,15 +2,17 @@ import express from "express"
 const app = express()
 
 app.use(express.json())
+
+let jwt=Math.floor(Math.random()*1001)+1000
+ let stringJwt=String(jwt)
+
 const auth=(req, res, next)=>{
 
-    const jwt="Math.floor(Math.random()*1001)+1000"
+   
 
-    const stringJwt=String(jwt)
+    let BearerToken=req.headers.authorization
 
-    const BearerToken=req.headers.authorization
-
-    const value=BearerToken.split(" ")
+    let value=BearerToken.split(" ")
 
     if(value[1]===stringJwt){
         next()  
