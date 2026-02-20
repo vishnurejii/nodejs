b995de29e7566afaf2e9bbd4be7003a2e050356e
vishnurejii <vishnurejii2003@gmail.com>
2026-02-20 15:48:15 +0530

    commit

--- index9a.js
+++ index9a.js
@@ -1,19 +1,39 @@
-import express from "express"
+// import express from "express"
 
+// const app = express()
+
+// const auth = (req, res, next) => {
+//     if (req.url === "/1234") {
+//         next()  
+//     } else {
+//         res.send("Stopped")
+//     }
+// }
+
+// app.use(auth)
+
+// app.get("/1234", (req, res) => {
+//     res.send("Welcome")
+// })
+
+// app.listen(8080)
+
+import express from "express"
 const app = express()
 
-const auth = (req, res, next) => {
-    if (req.url === "/1234") {
+app.use(express.json())
+const auth=(req, res, next)=>{
+
+    const token=req.body.token   
+    if (token===1234) {
         next()  
     } else {
-        res.send("Stopped")
+        return res.send("stopped")
     }
 }
 
-app.use(auth)
-
-app.get("/1234", (req, res) => {
-    res.send("Welcome")
+app.post("/token",auth,(req, res)=>{
+    res.send("Welcome User")
 })
 
 app.listen(8080)
