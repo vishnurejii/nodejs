962f40d3c204997ec5bca08a51fa7b394ace7074
vishnurejii <vishnurejii2003@gmail.com>
2026-02-23 13:14:19 +0530

    new commit

--- index13.js
+++ index13.js
@@ -1,14 +1,30 @@
-import express from "express"
-const app=express()
-app.use(express.json())
-
-app.listen(8080)
-const users=[]
-app.post("/signup",(req,res)=>{
-    const body=req.body;
-    users.push(body)
-    res.json(users)
-})
-app.get("/users",(req,res)=>{
-    res.json(users)
-})
+import express from "express";
+import bcrypt from "bcrypt";
+const app = express();
+app.use(express.json());
+app.listen(8080);
+const users = [];
+app.post("/signup", async (req, res) => {
+  const body = req.body;
+  const hashPassword = await bcrypt.hash(body.password, 10);
+  body.password = hashPassword;
+  users.push(body);
+  res.json(users);
+});
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;//destructing email and password only
+  const found = users.find((user) => user.email === email);
+  if (found) {
+    const chkPassword = await bcrypt.compare(password, found.password);
+    if (chkPassword) {
+      res.status(200).json({ message: "success" });
+    } else {
+      res.status(401).json({ message: "Invalid Password" });
+    }
+  } else {
+    res.status(401).json({ message: "User not found" });
+  }
+});
+app.get("/users", (req, res) => {
+  res.json(users);
+});
